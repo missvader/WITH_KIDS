c2720fe9df0981b7140d3d03337cce1ea29100bd
missvader <vanepalazon611@gmail.com>
2023-02-01 12:41:13 +0100

    solved challenge. All data includind geojson, and states are in context data

--- src/contexts/Data.jsx
+++ src/contexts/Data.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import { DataContext } from "./DataContext";
-
+import Restaurants from "../data/restaurants.json";
 
 const agenda = axios.create({
   baseURL:"https://analisi.transparenciacatalunya.cat/resource/rhpv-yr4f.json?$query=SELECT%0A%20%20%60codi%60%2C%0A%20%20%60data_fi%60%2C%0A%20%20%60data_inici%60%2C%0A%20%20%60data_fi_aproximada%60%2C%0A%20%20%60denominaci%60%2C%0A%20%20%60descripcio%60%2C%0A%20%20%60entrades%60%2C%0A%20%20%60horari%60%2C%0A%20%20%60subt_tol%60%2C%0A%20%20%60tags_mbits%60%2C%0A%20%20%60tags_categor_es%60%2C%0A%20%20%60tags_altres_categor_es%60%2C%0A%20%20%60enlla_os%60%2C%0A%20%20%60documents%60%2C%0A%20%20%60imatges%60%2C%0A%20%20%60v_deos%60%2C%0A%20%20%60adre_a%60%2C%0A%20%20%60codi_postal%60%2C%0A%20%20%60comarca_i_municipi%60%2C%0A%20%20%60email%60%2C%0A%20%20%60espai%60%2C%0A%20%20%60latitud%60%2C%0A%20%20%60localitat%60%2C%0A%20%20%60longitud%60%2C%0A%20%20%60regi_o_pa_s%60%2C%0A%20%20%60tel_fon%60%2C%0A%20%20%60url%60%2C%0A%20%20%60imgapp%60%2C%0A%20%20%60descripcio_html%60%0AWHERE%0A%20%20(%60comarca_i_municipi%60%0A%20%20%20%20%20%3D%20'agenda%3Aubicacions%2Fbarcelona%2Fbarcelones%2Fbarcelona')%0A%20%20AND%20(contains(%60tags_categor_es%60%2C%20'agenda%3Acategories%2Finfantil')%0A%20%20%20%20%20%20%20%20%20AND%20(%60data_fi%60%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20BETWEEN%20'2023-01-24T11%3A45%3A57'%20%3A%3A%20floating_timestamp%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20AND%20'2023-12-19T11%3A45%3A57'%20%3A%3A%20floating_timestamp))"
@@ -22,7 +22,10 @@ function Data({children}){
   const [parques, setParques] = useState([]);
   const [actividades, setActividades] = useState([]);
   const [actBiblio, setActBiblio] = useState([]);
-  
+  const [geoAgenda, setGeoAgenda] = useState([]);
+  const [geoBiblio, setGeoBiblio] = useState([]);
+  const [geoParques, setGeoParques] = useState([]);
+  const [geoRestaurantes, setGeoRestaurantes] = useState([]);
   /*FALTAN LOS RESTAURANTES */
 
   
@@ -33,7 +36,6 @@ function Data({children}){
       setActividades(response.data);
     }
     getActivities();
-    console.log(actividades)
   }, []); 
   useEffect(() => {
     async function getActBiblio() {
@@ -41,7 +43,6 @@ function Data({children}){
       setActBiblio(response.data.elements);
     }
     getActBiblio();
-    console.log(actBiblio)
   }, []); 
   useEffect(() => {
     async function getParques() {
@@ -49,9 +50,86 @@ function Data({children}){
       setParques(response.data.result.records);
     }
     getParques();
-    console.log(parques)
   }, []); 
-   
+  
+ useEffect(() => {
+    let geojsonAgenda = {
+      "type": "FeatureCollection",
+      "features": actividades.map(item => {
+        return {
+          "id": item.codi,
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              parseFloat(item.longitud),
+              parseFloat(item.latitud)
+              ]
+          },
+          "properties": {
+             "titol": item.denominac,
+             "espai": item.espai,
+             "inici": item.data_inici,
+             "fi": item.data_fi,
+          }
+        };
+      })
+    };
+    setGeoAgenda(geojsonAgenda);
+ }, [actividades])
+ useEffect(() => {
+  let geojsonBiblio = {
+    "type": "FeatureCollection",
+    "features": actBiblio.map(item => {
+      const coord = (item.grup_adreca.localitzacio).split(",");
+      return {
+        "id": item.acte_id,
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            parseFloat(coord[1]),
+            parseFloat(coord[0])
+          ]
+        },
+        "properties": {
+          "id": item.acte_id,
+          "titol": item.titol,
+          "organitzadors": item.acte_organitzadors,
+          "inici": item.data_inici,
+          "fi": item.data_fi,
+          "dies": item.dies
+        }
+      };
+    })
+   };
+   setGeoBiblio(geojsonBiblio);
+ }, [actBiblio]);
+ useEffect(() => {
+  let geojsonParques = {
+    "type": "FeatureCollection",
+    "features": parques.map(item => {
+      return {
+        "id": item._id,
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            parseFloat(item.Longitud),
+            parseFloat(item.Latitud)
+          ]
+        },
+        "properties": {
+          "id": item._id,
+        }
+      };
+    })
+  };
+  setGeoParques(geojsonParques);
+ }, [parques]);
+useEffect(() => {
+  setGeoRestaurantes({Restaurants})
+},[]);
 	// Retornamos el Provider con el estado que será global con la función que lo actualiza
 	return (
     <DataContext.Provider value={{
@@ -60,6 +138,10 @@ function Data({children}){
       parques,
       isLoading: true,
       userLocation: undefined,
+      geoAgenda,
+      geoBiblio,
+      geoParques,
+      geoRestaurantes
       }}>
       {children}
     </DataContext.Provider>);
