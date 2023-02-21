5e8b720c2a094182462ec6e4be5b855146dddae2
missvader <vanepalazon611@gmail.com>
2023-02-21 22:13:13 +0100

    favoriteList component renders favs onClick, only logic

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import FavoritesAgenda from "./FavoritesAgenda";
 import FavoritesBiblio from "./FavoritesBiblio";
 import FavoritesRest from "./FavoritesRest";
 
 const FavoriteList = () => {
-  /*Aqui va la logica de los 3 botones que renderizaran cada uno de los componentes de favoritos, pendiente tambien hacer los 3 botones  */
+  /*Aqui va la logica de los 3 botones que renderizaran cada uno de los componentes de favoritos */
+  const [selectedFav, setSelectedFav] = useState(null)
+  const selectComponent = (componentId) => {
+    switch(componentId){
+      case 1:
+        return <FavoritesAgenda/>;
+      case 2: 
+        return <FavoritesBiblio/>;
+      case 3: 
+        return <FavoritesRest/>;
+      default:
+        return null;
+    }
+  }
+
   return (
     <div>
-        <FavoritesAgenda/>
-        <FavoritesBiblio/>
-        <FavoritesRest/>
+        <button className="p-4" onClick={()=>setSelectedFav(1)}>AGENDA</button>
+        <button className="p-4" onClick={()=>setSelectedFav(2)}>BIBLIOS</button>
+        <button className="p-4" onClick={()=>setSelectedFav(3)}>RESTAURANTEs</button>
+        {selectComponent(selectedFav)}
     </div>
   )
 }
